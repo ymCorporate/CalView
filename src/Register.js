b721d54c90bf8bd2f0299eaa49827659a797ee0a
yashas <yashas105@gmail.com>
2024-05-06 15:26:17 +0530

    signup-form Registration input implemented

---
+++ src/Register.js
@@ -0,0 +1,89 @@
+import { useRef,useState,useEffect} from "react";
+import { faCheck,faTimes,faInfoCircle} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const USER_REGX = /^[a-zA-Z][a-zA-Z0-9_]{3,23}$/;
+const PWD_REGX =/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+const Register = ()=>{
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user,setUser] = useState('')
+    const [validName,setValidName]=useState(false)
+    const [userFocus,setUserFocus]=useState(false)
+
+    const [pwd,setPwd]=useState('');
+    const [validPwd,setValidPwd]=useState(false)
+    const [pwdFocus,setPwdFocus]=useState(false)
+
+    const [matchPwd,setMatchPwd]=useState('')
+    const [validMatch,setValidMatch]=useState(false)
+    const [matchFocus,setMatchFocus]=useState(false)
+
+    const [errMsg,setErrMsg]= useState('')
+    const [success,setSuccess]=useState(false)
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(()=>{
+        const result = USER_REGX.test(user)
+        console.log(user)
+        console.log(result)
+        setValidName(result);
+    },[user])
+
+    useEffect(() => {
+        const result = USER_REGX.test(pwd)
+        console.log(pwd)
+        console.log(result)
+        setValidPwd(result)
+        const match = pwd === matchPwd
+        setValidMatch(match)
+    }, [pwd,matchPwd]);
+
+    useEffect(()=>{
+        setErrMsg('');
+    },[user,pwd,matchPwd])
+    return(
+        <section>
+            <p ref={errRef} className={errMsg ? "errmsg":"offscreen"}
+               aria-live="assertive">{errMsg}</p>
+            <h1>Register</h1>
+            <form>
+                <label htmlFor="username">
+                    Username:
+                    <span className={validName ? "valid":"hide"}>
+                        <FontAwesomeIcon icon={faCheck}/>
+                    </span>
+                    <span className={validName || !user ? "hide":"invalid"}>
+                        <FontAwesomeIcon icon={faTimes}/>
+                    </span>
+                </label>
+                <input
+                    type="text"
+                    id="username"
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(e)=>setUser(e.target.value)}
+                    required
+                    aria-invalid={validName ? "false":"true"}
+                    aria-describedy="uidnote"
+                    onFocus={()=>setUserFocus(true)}
+                    onBlur={()=>setUserFocus(false)}
+                />
+                <p id="uidnote" className={userFocus && user && !validName ? "instructions":"offscreen"}>
+                    < FontAwesomeIcon icon = {faInfoCircle}/>
+                    4 to 24 characters<br/>
+                    Must begin with a letter<br/>
+                    Letters,Numbers,underscores,hyphens allowed
+                </p>
+
+            </form>
+        </section>
+    )
+}
+
+export default Register
