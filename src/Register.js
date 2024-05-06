b45a4f8c92c68d107bea3ee6093ee932fcec3ee1
yashas <yashas105@gmail.com>
2024-05-06 16:38:32 +0530

    Implemented password input

--- src/Register.js
+++ src/Register.js
@@ -70,7 +70,7 @@ const Register = ()=>{
                     onChange={(e)=>setUser(e.target.value)}
                     required
                     aria-invalid={validName ? "false":"true"}
-                    aria-describedy="uidnote"
+                    aria-describedby="uidnote"
                     onFocus={()=>setUserFocus(true)}
                     onBlur={()=>setUserFocus(false)}
                 />
@@ -79,6 +79,35 @@ const Register = ()=>{
                     4 to 24 characters<br/>
                     Must begin with a letter<br/>
                     Letters,Numbers,underscores,hyphens allowed
+                </p>
+                <label htmlFor="password">
+                    Password:
+                    <span className={validPwd ? "valid":"hide"}>
+                        <FontAwesomeIcon icon={faCheck}/>
+                    </span>
+                    <span className={validPwd || !pwd ? "hide":"invalid"}>
+                        <FontAwesomeIcon icon={faTimes}/>
+                    </span>
+                </label>
+                <input
+                    type="password"
+                    id="password"
+                    onChange={(e)=>setPwd(e.target.value)}
+                    required
+                    aria-invalid={ validPwd ? "false":"true"}
+                    aria-describedby="pwdnote"
+                    onFocus={()=>{setPwdFocus(true)}}
+                    onBlur={()=>{setPwdFocus(false)}}
+                />
+                <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+                    <FontAwesomeIcon icon={faInfoCircle}/>
+                    8 to 24 Characters<br/>
+                    Must begin with a letter,Must include<br/>
+                    One Uppercase,one Lowercase and one special Character<br/>
+                    Allowed special characters:<span aria-label="exclaimaitory-mark">!</span>
+                    <span aria-label="exclamation mark">!</span>
+                    <span aria-label="at the rate">@</span><span aria-label="hash tag">#</span>
+                    <span aria-label="dollar sign">$</span><span aria-label="percentage">%</span>
                 </p>
 
             </form>
