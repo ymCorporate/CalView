653bceda3858cc8c792e9b7588e2357a6c36169c
yashas <yashas105@gmail.com>
2024-05-09 10:11:53 +0530

    Auth0 implementation

---
+++ src/components/Logout.js
@@ -0,0 +1,12 @@
+import {Auth0Provider, useAuth0} from "@auth0/auth0-react";
+
+const LogoutButton = () => {
+    const {logout,isAuthenticated} = useAuth0();
+    return(
+        isAuthenticated && (
+            <button onClick={()=>logout()}>Logout</button>
+        )
+    )
+}
+
+export default LogoutButton;
